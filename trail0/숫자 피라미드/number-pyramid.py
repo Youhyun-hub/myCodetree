242adda6006de6f1fe216db1fd1fe72c6f496f87
Youhyun-hub <cy970225@gmail.com>
2026-07-25 16:05:47 +0900

    [Passed] 숫자 피라미드 (48ms, 16MB)

---
+++ trail0/숫자 피라미드/number-pyramid.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(i, end=" ")
+    print()
+
+# for i in range(1,2):
+#     print(i)
+# for i in range(2,3):
+#     for j in range(1, i+1):
+#         print(i, end=" ")
+#     print()
+# for i in range(3,4):
+#     for j in range(1, i+1):
+#         print(i, end=" ")
+#     print()
+# for i in range(4,5):
+#     for j in range(1, i+1):
+#         print(i, end=" ")
+#     print()
+# for i in range(5,6):
+#     for j in range(1, i+1):
+#         print(i, end=" ")
+#     print()

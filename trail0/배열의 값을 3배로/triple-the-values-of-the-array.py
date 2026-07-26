0b3eaac59e54abf440c8f827b21ec308e40effac
Youhyun-hub <cy970225@gmail.com>
2026-07-26 14:33:23 +0900

    [Passed] 배열의 값을 3배로 (50ms, 16MB)

--- trail0/배열의 값을 3배로/triple-the-values-of-the-array.py
+++ trail0/배열의 값을 3배로/triple-the-values-of-the-array.py
@@ -11,10 +11,10 @@ for i in range(3):
         print(matrix[i][j], end = " ")
     print()
 
-# 3줄의 입력을 받아 각각 3배를 한 뒤 바로 출력
-for _ in range(3):
-    # 1. 입력받은 수들을 각각 3배하여 리스트 생성 (리스트 컴프리핸션)
-    row = [x * 3 for x in map(int, input().split())]
+# # 3줄의 입력을 받아 각각 3배를 한 뒤 바로 출력
+# for _ in range(3):
+#     # 1. 입력받은 수들을 각각 3배하여 리스트 생성 (리스트 컴프리핸션)
+#     row = [x * 3 for x in map(int, input().split())]
     
-    # 2. 리스트 요소를 공백으로 구분하여 한 줄 출력 (* 언패킹)
-    print(*row)
+#     # 2. 리스트 요소를 공백으로 구분하여 한 줄 출력 (* 언패킹)
+#     print(*row)

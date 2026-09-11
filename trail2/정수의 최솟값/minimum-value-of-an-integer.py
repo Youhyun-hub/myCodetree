866eef8ff4ebe8e19e7a55bb2f64646c9180e569
Youhyun-hub <cy970225@gmail.com>
2026-09-11 09:32:33 +0900

    [Passed] 정수의 최솟값 (47ms, 16MB)

--- trail2/정수의 최솟값/minimum-value-of-an-integer.py
+++ trail2/정수의 최솟값/minimum-value-of-an-integer.py
@@ -2,9 +2,9 @@ a, b, c = map(int, input().split())
 
 def min_val(i, j, k):
     min_v = i
-    if i > j:
+    if j < min_v:
         min_v = j
-    elif i > k:
+    if k < min_v:
         min_v = k
     return min_v
 

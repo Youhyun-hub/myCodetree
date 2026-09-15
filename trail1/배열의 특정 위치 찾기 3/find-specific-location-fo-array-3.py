f981e30ad66184911fd4704164ef1c92d489fb7a
Youhyun-hub <cy970225@gmail.com>
2026-09-15 17:08:57 +0900

    [Passed] 배열의 특정 위치 찾기 3 (50ms, 16MB)

--- trail1/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ trail1/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -1,13 +1,9 @@
 arr = list(map(int, input().split()))
 
 total = 0
-for i in range(len(arr)):
+for i in range(3, len(arr)):
     if arr[i] == 0:
-        if i <= 3:
-            total = sum(arr[:i])
-            break
-        else:
-            total = sum(arr[i-3:i])
-            break
+        total = sum(arr[i-3:i])
+        break
     
 print(total)

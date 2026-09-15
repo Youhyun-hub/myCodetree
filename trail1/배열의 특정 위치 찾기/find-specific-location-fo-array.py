9509fa113fc073c94567691bfed5b54adcf0261d
Youhyun-hub <cy970225@gmail.com>
2026-09-15 17:00:27 +0900

    [Wrong Answer] 배열의 특정 위치 찾기 (47ms, 16MB)

--- trail1/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ trail1/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -3,14 +3,17 @@ arr = list(map(int, input().split()))
 s = 0
 v = []
 a = 0
+
 for i in range(len(arr)):
-    if arr[i] % 2 == 0:
+    if i % 2 != 0:
         s += arr[i]
 
-    elif (i + 1) % 3 == 0:
+    if (i + 1) % 3 == 0:
         v.append(i)
-        for j in v:
-            a += arr[j]
-            a = a / len(v)
 
-print(s, a)    
+for j in v:
+    a += arr[j]
+
+a = a / len(v)
+
+print(s, a)

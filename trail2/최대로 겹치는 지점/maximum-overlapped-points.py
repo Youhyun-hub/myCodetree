8f8d8f44d2e7f6580246a9833816904d3d9e2770
Youhyun-hub <cy970225@gmail.com>
2026-09-16 16:56:43 +0900

    [Runtime Error] 최대로 겹치는 지점 (85ms, 18MB)

---
+++ trail2/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,10 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+arr = [0] * 100
+
+cnt = 0
+for a, b in segments:
+    for i in range(a, b+1):
+        arr[i] += 1
+    cnt = max(arr)
+print(cnt)

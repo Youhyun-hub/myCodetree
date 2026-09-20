ba1c2a8e6b9b434dbcc9f08b1ac2f7b86637d50a
Youhyun-hub <cy970225@gmail.com>
2026-09-20 18:33:29 +0900

    [Wrong Answer] 흰검 칠하기 (52ms, 16MB)

--- trail2/흰검 칠하기/painting-white-black.py
+++ trail2/흰검 칠하기/painting-white-black.py
@@ -8,12 +8,14 @@ for num, direction in commands:
 
 # L => 흰색, R => 검은색, 겹치는 구간 => 회색
 cur = 0  # 현 위치
-tiles = [[0] * (2*sum(x)+1) for _ in range(n)]
+# tiles = [[0] * (2*sum(x)+1) for _ in range(n)]
+tile = [0] * (2*sum(x)+1) # 타일의 현재 색만 담음
 
-# 순서 따지기 & 회색으로 바꾸기
+# 색을 칠할때마다 카운트
 white = [0] * (2*sum(x)+1)  # 흰색
 black = [0] * (2*sum(x)+1)  # 검은색
-gray = [0] * (2*sum(x)+1)  # 회색
+# gray = [0] * (2*sum(x)+1) 
+
 
 for i in range(n):
     if dir[i] == "L":  # 왼쪽
@@ -21,8 +23,15 @@ for i in range(n):
         end = cur - x[i]
         cur = end
         for j in range(min(start, end), max(start, end)):
+            idx = j + sum(x)
             # 각 두번 이상 나오지 않으면
-            if white[i] + black[i] < 3:
+            if white[idx] >= 2 and + black[idx] >= 2:
+                tile[idx]= "gray"
+                # gray[idx] += 1
+            else:
+                tile[idx] = "white" # 현재 타일 색
+                white[idx] += 1  # 누적 칠해진 카운트
+                
                
 
     else:  # 오른쪽
@@ -30,22 +39,30 @@ for i in range(n):
         end = cur + x[i]
         cur = end
         for j in range(min(start, end), max(start, end)):
-            tiles[i][j+sum(x)] = "black"
+            idx = j + sum(x)
+            # 각 두번 이상 나오지 않으면
+            if white[idx] >= 2 and + black[idx] >= 2:
+                tile[idx]= "gray"
+                # gray[idx] += 1   
+            else:
+                tile[idx] = "black" # 현재 타일 색
+                white[idx] += 1  # 누적 칠해진 카운트
 
-print(tiles)
 
-for i in range(n):
-    for j in range(len(tiles[i])):
-        if tiles[i][j] == "white":
-            cnt_w[j] += 1
-        elif tiles[i][j] == "black":
-            cnt_b[j] += 1
+# for i in range(n):
+#     for j in range(len(tiles[i])):
+#         if tiles[i][j] == "white":
+#             cnt_w[j] += 1
+#         elif tiles[i][j] == "black":
+#             cnt_b[j] += 1
+#         else:
+#             cnt_
         
-        # # 회색 처리
-        # if cnt_w[j] >= 2 and cnt_b[j] >= 2:
-        #     cnt_g[j] += 1
-        # elif cnt_w[j] < 2 and cnt_b[j] < 2: # 다음에 나온 색으로 바꾸기
-        #     if cnt_w[j] !=0 and cnt_b[j] != 0:
-print(cnt_w, cnt_b)
+#         # # 회색 처리
+#         # if cnt_w[j] >= 2 and cnt_b[j] >= 2:
+#         #     cnt_g[j] += 1
+#         # elif cnt_w[j] < 2 and cnt_b[j] < 2: # 다음에 나온 색으로 바꾸기
+#         #     if cnt_w[j] !=0 and cnt_b[j] != 0:
+# print(cnt_w, cnt_b)
     
-# print(cnt_w, cnt_b, cnt_g)
+print(tile.count("white"), tile.count("black"), tile.count("gray"))
